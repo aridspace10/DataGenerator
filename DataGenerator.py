389728736950944bca6166dc59388041533219e1
aridspace10 <jackovand27@icloud.com>
2024-03-29 10:57:07 +1000

    Added functionality to combine first name and last name

--- DataGenerator.py
+++ DataGenerator.py
@@ -1,6 +1,6 @@
 import random
 
-AVAILABLE_TYPES = ['fname', 'lname']
+AVAILABLE_TYPES = ['fname', 'lname', 'full_name']
 class DataGenerator():
     def __init__(self):
         pass
@@ -14,19 +14,25 @@ class DataGenerator():
     """
     def simple_generate(self, type: str, n: int) -> list:
         type = type.lower()
+        processed_data = []
         if type not in AVAILABLE_TYPES:
             raise ValueError("Type must be in " + str(AVAILABLE_TYPES))
         else:
-            with open(type + ".txt", "r") as f:
-                raw_data = [x.strip() for x in f.readlines()]     
-                if n > len(raw_data):
-                    raise ValueError("n must be less than " + str(len(raw_data)))       
-                processed_data = []
-                while len(processed_data) < n:
-                    data = (random.choice(raw_data))
-                    if data not in processed_data:
-                        processed_data.append(data)
+            if type != 'full_name':
+                with open(type + ".txt", "r") as f:
+                    raw_data = [x.strip() for x in f.readlines()]     
+                    if n > len(raw_data):
+                        raise ValueError("n must be less than " + str(len(raw_data)))       
+                    while len(processed_data) < n:
+                        data = (random.choice(raw_data))
+                        if data not in processed_data:
+                            processed_data.append(data)
+            else:
+                fname = self.simple_generate('fname', n)
+                lname = self.simple_generate('lname', n)
+                for i in range(n):
+                    processed_data.append(fname[i] + ' ' + lname[i])
             return processed_data
 
 dataGenerator = DataGenerator()
-print(dataGenerator.simple_generate('lname', 10))
+print(dataGenerator.simple_generate('full_name', 10))
